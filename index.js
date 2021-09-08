86ad7a3c594f0d6797955c4ba9b46a557c92b19d
GabytaDev <gabyta.loyola@gmail.com>
2021-09-08 13:59:48 -0300

    terminando modo oscuro

--- index.js
+++ index.js
@@ -94,3 +94,66 @@ iconCloseMenu.onclick = () => {
     menuMobile.style.display = ("none")
 }
 
+// Modo Oscuro//
+
+const btnModoOscuro = document.getElementById("boton-modo-oscuro")
+const btnModoOscuroMobile = document.getElementById("boton-modo-oscuro-mobile")
+
+const fondoBody = document.querySelector("body"); //fondo cambiar a gris
+const seccionFondo = document.querySelector(".seccion-fondo"); //fondo cambiar a oscuro
+const nav = document.querySelector(".nav");
+const titulo = document.querySelectorAll(".titulo");
+const subtitulos = document.querySelectorAll(".subtitulos"); //textos cambiar a blanco
+const centerTexto = document.querySelectorAll(".center-texto");
+const textoArticulo = document.querySelector(".texto-articulo");
+const textoFooter = document.querySelector("footer > h3");
+const textoContacto = document.querySelector(".texto-contacto");
+const colorIconMail = document.querySelector(".fa-envelope")
+const colorIconPhone = document.querySelector(".fa-phone")
+const colorIconLinkedin = document.querySelector(".fa-linkedin")
+const colorIconoMenu = document.querySelector("#icon-menu-mobile");
+
+const cambioColorTitulo = (titulo) => {
+    for (let i = 0; i < titulo.length; i++) {
+        titulo[i].classList.toggle("fuentes-modo-oscuro")  
+    }   
+  console.log(titulo) 
+}
+
+const cambioColorSubtitulos = (subtitulos) => {
+    for (let i = 0; i < subtitulos.length; i++) {
+        subtitulos[i].classList.toggle("fuentes-modo-oscuro") 
+    }   
+  console.log(subtitulos) 
+}
+
+const cambioColorTexto = (centerTexto) => {
+    for (let i = 0; i < centerTexto.length; i++) {
+        centerTexto[i].classList.toggle("fuentes-modo-oscuro") 
+        
+    }
+    console.log(centerTexto) 
+}
+
+const cambiarModoOscuro = () => {
+    fondoBody.classList.toggle("clase-modo-oscuro")
+    seccionFondo.classList.toggle("clase-modo-oscuro")
+    textoArticulo.classList.toggle("fuentes-modo-oscuro")
+    textoFooter.classList.toggle("fuentes-modo-oscuro")
+    textoContacto.classList.toggle("fuentes-modo-oscuro")
+    nav.classList.toggle("fondo-modo-oscuro")
+    colorIconMail.classList.toggle("fuentes-modo-oscuro")
+    colorIconPhone.classList.toggle("fuentes-modo-oscuro")
+    colorIconLinkedin.classList.toggle("fuentes-modo-oscuro")
+    colorIconoMenu.classList.toggle("fuente-modo-oscuro-mobile")
+    cambioColorTitulo(titulo)
+    cambioColorSubtitulos(subtitulos)
+    cambioColorTexto(centerTexto)
+}
+
+
+
+
+
+btnModoOscuro.onclick = cambiarModoOscuro;
+btnModoOscuroMobile.onclick = cambiarModoOscuro;
